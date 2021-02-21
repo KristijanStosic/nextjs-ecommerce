edc8b2b7b61f527336ab2cd74941c0b0ba0eefa9
Kristijan19 <nimeria83@yahoo.com>
2021-02-21 11:09:01 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    BASE_URL: 'http://localhost:3000',
+    MONGODB_URL:
+      'mongodb+srv://admin123:admin123@cluster0.wh5c4.mongodb.net/next_ecommerce?retryWrites=true&w=majority',
+    ACCESS_TOKEN_SECRET: 'admin12311924297978236037',
+    REFRESH_TOKEN_SECRET: 'admin123secret53570461rere91753131',
+    PAYPAL_CLIENT_ID:
+      'Af96WU8dAbfM8yutKHJPmfA5llBpc3-2V1ZHGZotAAk_9jAUvlwgOEXuyjjnwd0HIbQpDpXIcEbEWM7k',
+    CLOUD_UPDATE_PRESET: 'nextjs_store',
+    CLOUD_NAME: 'kristijan',
+    CLOUD_API: 'https://api.cloudinary.com/v1_1/kristijan/image/upload',
+  },
+}
